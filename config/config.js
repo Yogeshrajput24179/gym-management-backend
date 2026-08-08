262e3ac879acee7aa836c1b7d19b1ea46a9546d2
yogesh <yogesh@ecaresoftech.com>
2026-08-08 15:58:13 +0530

    update config file

--- config/config.js
+++ config/config.js
@@ -1,4 +1,3 @@
-// config/config.js
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +8,22 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL", // Uses the full Service URI from Aiven
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 25504,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Required for Aiven SSL connection
+      },
+    },
   },
 };
